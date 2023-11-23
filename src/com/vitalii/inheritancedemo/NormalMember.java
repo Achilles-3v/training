aa37383209300d0117f7e896e8719d7b1acf3e39
Achilles-3v <sh17vv@gmail.com>
2023-11-24 04:11:23 +0500

    add class NormalMember

--- src/com/vitalii/inheritancedemo/NormalMember.java
+++ src/com/vitalii/inheritancedemo/NormalMember.java
@@ -1,5 +1,13 @@
 package com.vitalii.inheritancedemo;
 
 public class NormalMember extends Member {
+    public NormalMember(String pName, int pMemberID, int pMemberSince) {
+        super(pName, pMemberID, pMemberSince);
+        System.out.println("Child Constructor with 3 parameters");
+    }
 
+    @Override
+    public void calculateAnnualFee () {
+        annualFee = (1 - 0.01 * getDiscount()) * (100 + 12 * 30);
+    }
 }
