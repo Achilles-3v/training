5baf9a79a0e35f3df03268087832067798c55bfc
Achilles-3v <sh17vv@gmail.com>
2023-11-26 01:35:08 +0500

    another implementation of the method main

--- src/com/vitalii/inheritancedemo/InheritanceDemo.java
+++ src/com/vitalii/inheritancedemo/InheritanceDemo.java
@@ -2,16 +2,18 @@ package com.vitalii.inheritancedemo;
 
 public class InheritanceDemo {
     public static void main(String[] args) {
-        NormalMember mem1 = new NormalMember("James", 1, 2010);
-        VIPMember mem2 = new VIPMember("Andy", 2, 2011);
+        Member[] clubMember = new Member[6];
 
-        mem1.calculateAnnualFee();
-        mem2.calculateAnnualFee();
-        mem1.displayMemInfo();
-        mem2.displayMemInfo();
+        clubMember[0] = new NormalMember("James", 1, 2010);
+        clubMember[1] = new NormalMember("Andy", 2, 2011);
+        clubMember[2] = new NormalMember("Bill", 3, 2011);
+        clubMember[3] = new VIPMember("Carol", 4, 2012);
+        clubMember[4] = new VIPMember("Evelyn", 5, 2012);
+        clubMember[5] = new Member("Yvonne", 6, 2013);
 
-        mem1.setDiscount();
-        mem1.calculateAnnualFee();
-        mem1.displayMemInfo();
+        for (Member m: clubMember) {
+            m.calculateAnnualFee();
+            m.displayMemInfo();
+        }
     }
 }
