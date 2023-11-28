628055fe5fd358d725118af2ee709ed77b30905c
Achilles-3v <sh17vv@gmail.com>
2023-11-28 05:01:48 +0500

    create project filehandlingdemo

---
+++ src/com/vitalii/filehandlingdemo/FileHandlingDemo.java
@@ -0,0 +1,30 @@
+package com.vitalii.filehandlingdemo;
+import java.io.*;
+
+public class FileHandlingDemo {
+    public static void main(String[] args) {
+        String line;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader("src//com//vitalii//filehandlingdemo//myFile.txt"));
+            line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            }
+            catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
