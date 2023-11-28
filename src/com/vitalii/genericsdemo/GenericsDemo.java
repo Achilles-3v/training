d20622a9ef997970275d5973cc2926af6d32e810
Achilles-3v <sh17vv@gmail.com>
2023-11-29 00:02:06 +0500

    create a class generics

---
+++ src/com/vitalii/genericsdemo/GenericsDemo.java
@@ -0,0 +1,21 @@
+package com.vitalii.genericsdemo;
+
+public class GenericsDemo {
+    public static void main(String[] args) {
+        MyGenericsClass g = new MyGenericsClass();
+
+        g.setMyVar(6);
+        g.printValue();
+    }
+}
+
+class MyGenericsClass {
+    private Integer myVar;
+
+    void setMyVar(Integer i) {
+        myVar = i;
+    }
+    void printValue() {
+        System.out.println("The value of myVar is " + myVar);
+    }
+}
